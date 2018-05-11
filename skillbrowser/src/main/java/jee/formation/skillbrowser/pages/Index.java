899fcc6d5392e8555f6340f293cff7fdc4caaa60
DELL <DELL@DESKTOP-2878OKN>
2018-05-11 17:55:34 +0200

    first commit

---
+++ skillbrowser/src/main/java/jee/formation/skillbrowser/pages/Index.java
@@ -0,0 +1,14 @@
+package jee.formation.skillbrowser.pages;
+
+import java.util.Date;
+
+/**
+ * Start page of application skillbrowser.
+ */
+public class Index
+{
+	public Date getCurrentTime() 
+	{ 
+		return new Date(); 
+	}
+}
